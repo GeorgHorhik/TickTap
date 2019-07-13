f20dc27f56e9fe5b8f6e5671d85f8cace8344cca
GeorgHorhik <horhi20032003@gmail.com>
2019-07-13 13:48:59 +0300

    commit for sync

---
+++ app/js/index.js
@@ -0,0 +1,15 @@
+const
+    createTaskButton = document.querySelector(".create-task-button"), addTaskButton = document.querySelector("create-input__add-task"),
+    inputHeading = document.querySelector(".create-input__heading"),
+    inputDescription = document.querySelector(".create-input__description"),
+    seearchBar = document.querySelector(".search"),
+    seearchBarButton = document.querySelector(".search__button"),
+    tagsArea = document.querySelector(".existing-tags "),
+    tagsAreaItem = document.querySelector("exiting-tags__item");
+
+
+
+//TODO createTaskButton
+
+
+
